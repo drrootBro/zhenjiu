b2f35cac5b9aad05482129854da976f691a240af
drrootBro <firstvoteguy@gmail.com>
2025-07-26 18:51:57 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -70,10 +70,3 @@ function showDetails(symptom, clickedButton) {
 }
 
 window.addEventListener('DOMContentLoaded', renderPage);
-let html = `
-  <div class="result">
-    <h3 style="color:#2f7a5f;">你选择的症状：${symptom}</h3>
-    ...
-</div>
-`;
-
